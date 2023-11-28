f56a7e9f16fa8fa46f22a7260034e9a8cc12a960
Thomas Lu <149342460+tlu2004@users.noreply.github.com>
2023-11-27 19:36:58 -0800

    datasets

---
+++ data.R
@@ -0,0 +1,11 @@
+library(dplyr)
+library(stringr)
+
+# Loading Data Sets
+
+finance_df <- read.csv("DistrictCostDatabase_2023.csv")
+scores_df <- read.csv("seda2022_admindist_poolsub_ys_2.0.csv")
+
+# Data Wrangling and Cleaning
+
+
